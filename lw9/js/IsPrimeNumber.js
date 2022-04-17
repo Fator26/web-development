7235c861ad0f3b64178d27e4f5ac6ba6f6656d0e
Fator26 <fator26@yandex.ru>
2022-04-17 15:20:12 +0300

    добавлена обработка не числового значения или не массива

--- lw9/js/IsPrimeNumber.js
+++ lw9/js/IsPrimeNumber.js
@@ -1,28 +1,38 @@
 function isPrimeNumber(n) {
-    let isPrime;
-    let numberSet = [];
-
-    if (n !== undefined) {
-        if (!Array.isArray(n)) numberSet.push(n);
-        else numberSet = n;
-
-        for (let i = 0; i < numberSet.length; i++ ) {
-            isPrime = true;
-            if (Number.isInteger(numberSet[i]) && numberSet[i] > 1) {
-                for (let j = 2; j <= Math.round(Math.sqrt(numberSet[i])); j++) {
-                    if ((numberSet[i] % j === 0) || !Number.isInteger(numberSet[i])) {
-                        isPrime = false;
-                        // console.log(numberSet[i], ' is not prime number');
-                        break;
-                    }
-                }
-            }
-            else {
-                isPrime = false;
-            }
-
-            if (isPrime) console.log(numberSet[i], ' is prime number');
-            else  console.log(numberSet[i], ' is not prime number');
-        }
+  let isPrime;
+  let numberSet = [];
+  if (n !== undefined) {
+    if (Number.isInteger(n)) {
+      numberSet.push(n);
     }
+    else if (Array.isArray(n)) {
+      numberSet = n;
+    }
+    else {
+      console.log(n, 'is not number or array');
+      numberSet = [];
+    }
+    for (let i = 0; i < numberSet.length; i++ ) {
+      isPrime = true;
+      if (Number.isInteger(numberSet[i])) {
+        if (numberSet[i] > 1) {
+          for (let j = 2; j <= Math.round(Math.sqrt(numberSet[i])); j++) {
+            if (numberSet[i] % j === 0) {
+              isPrime = false;
+              break;
+            }
+          }
+        }
+        else {
+        isPrime = false;
+        }
+      }
+      else {
+        console.log(numberSet[i], 'is not number');
+        continue;
+      }
+      if (isPrime) console.log(numberSet[i], ' is prime number');
+      else  console.log(numberSet[i], ' is not prime number');
+    }
+  }
 }
