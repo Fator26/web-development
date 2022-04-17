0fbbd36a9de31b73c91b0776da0af5e2f019ba39
Fator26 <fator26@yandex.ru>
2022-04-17 20:58:32 +0300

    правки форматирования

--- lw9/js/calc.js
+++ lw9/js/calc.js
@@ -1,106 +1,85 @@
+function stringToArray(str) {
+  let returnedArr = '';
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] == '(' || str[i] == ')') returnedArr += ' ' + str[i] + ' ';
+    else  returnedArr += (str[i]);
+  }
+  returnedArr = returnedArr.split(' ');
+  returnedArr = returnedArr.filter(Boolean);
+  for (let i = 0; i < returnedArr.length; ++i)
+  {
+    if (Number(returnedArr[i])) returnedArr[i] = Number(returnedArr[i]);
+  }
+  return returnedArr;
+}
+
+function splitAndDoExpression(expression) {
+  let a, b;
+
+  b = readNewArray(expression);
+  if (Array.isArray(b)) b = splitAndDoExpression(b);
+
+  a = readNewArray(expression);
+  if (Array.isArray(a)) a = splitAndDoExpression(a);
+
+  if (a && b){
+    if (expression[0] == '/'){
+      if (b != 0)
+        return a / b;
+      else {
+        console.log("Division by 0!!!")
+        return false;
+      }
+    }
+    if (expression[0] == '*') return a * b;
+    if (expression[0] == '-') return a - b;
+    if (expression[0] == '+') return a + b;
+  }
+  console.log("Invalid expression.");
+  return false;
+}
+
+function readNewArray(arr) {
+  let returnedArr = [];
+  let bracketCounter = 0;
+  let arraySize;
+  let buffer;
+  if (arr[(arr.length - 1)] != ')') {
+    buffer = arr.pop();
+    return buffer;
+  }
+  else {
+    bracketCounter = 1;
+    arr.pop();
+    arraySize = arr.length;
+    while (bracketCounter != 0) {
+      arraySize--;
+      if (arraySize == 0) {
+        console.log('Invalid brackets');
+        return false;
+      }
+      if (arr[arraySize] == '(') bracketCounter--;
+      if (arr[arraySize] == ')') bracketCounter++;
+      buffer = arr.pop();
+      if (bracketCounter != 0) {
+        returnedArr.unshift(buffer);
+      }
+    }
+  }
+  return returnedArr;
+}
+
 function calc(str) {
-  let expretion = [];
+  let expression = [];
   if (str !== undefined) {
-    expretion = stringToArray(str);
-    expretion = splitAndDoExpretion(expretion);
-    if (expretion)
-      console.log(expretion);
+    expression = stringToArray(str);
+    expression = splitAndDoExpression(expression);
+    if (expression !== false)
+      console.log(expression);
   }
   else
   {
     console.log('String is empty');
     return false;
   }
-}
-
-function stringToArray(str) {
-  let returnedArr = '';
-  for (let i = 0; i < str.length; ++i) {
-    if (str[i] == '(' || str[i] == ')')
-      returnedArr += ' ' + str[i] + ' ';
-    else
-      returnedArr += (str[i]);
-  }
-  returnedArr = returnedArr.split(' ');
-  returnedArr = returnedArr.filter(Boolean);
-  for (let i = 0; i < returnedArr.length; ++i)
-  {
-    if (Number(returnedArr[i])){
-      returnedArr[i] = Number(returnedArr[i]);
-    }
-  }
-  return returnedArr;
-}
-
-function splitAndDoExpretion(expretion) {
-  let a, b;
-  let aArr = [];
-  let bArr = [];
-  let bracketCounter = 0;
-  let arraySize;
-  let buffer;
-
-  if (expretion[expretion.length - 1] != ')') {
-    b = expretion.pop(expretion[expretion.length - 1])
-  }
-  else {
-    bracketCounter = 1;
-    expretion.pop(expretion[expretion.length - 1]);
-    while (bracketCounter != 0) {
-      arraySize = expretion.length - 1;
-      if (arraySize == 0) {
-        console.log('Invalid bracket')
-        return false;
-      }
-      if (expretion[arraySize] == '(') bracketCounter--;
-      if (expretion[arraySize] == ')') bracketCounter++;
-      buffer = expretion.pop(expretion[arraySize]);
-      if (bracketCounter != 0) {
-        bArr.unshift(buffer);
-      }
-    }
-    b = splitAndDoExpretion(bArr);
-  }
-
-  if (expretion[expretion.length - 1] != ')') {
-    a = expretion.pop(expretion[expretion.length - 1])
-  }
-  else {
-    bracketCounter = 1;
-    expretion.pop(expretion[expretion.length - 1]);
-    while (bracketCounter != 0) {
-      arraySize = expretion.length - 1;
-      if (arraySize == 0) {
-        console.log('Invalid bracket')
-        return false;
-      }
-      if (expretion[arraySize] == '(') bracketCounter--;
-      if (expretion[arraySize] == ')') bracketCounter++;
-      buffer = expretion.pop(expretion[arraySize]);
-      if (bracketCounter != 0) {
-        aArr.unshift(buffer);
-      }
-    }
-    a = splitAndDoExpretion(aArr);
-  }
-  if (a && b){
-    if (expretion[0] == '/'){
-      if (b != 0)
-        return a / b;
-      else {
-        console.log("Деление на 0!!!")
-        return false;
-      }
-    }
-    if (expretion[0] == '*'){
-      return a * b;
-    }
-    if (expretion[0] == '-'){
-      return a - b;
-    }
-    if (expretion[0] == '+'){
-      return a + b;
-    }
-  }
-  return 'my poteryaly znak';
 }
