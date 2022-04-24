a2867ae23aff3012bb903552051e02d150a78a02
Fator26 <fator26@yandex.ru>
2022-04-24 19:43:00 +0300

    исправил ошибку при некорректном количестве операторов исправил подход решения задачи

--- lw9/js/calc.js
+++ lw9/js/calc.js
@@ -1,84 +1,105 @@
 function stringToArray(str) {
   let returnedArr = '';
+  let bracketCounter = 0;
   for (let i = 0; i < str.length; ++i) {
-    if (str[i] == '(' || str[i] == ')') returnedArr += ' ' + str[i] + ' ';
+    if (str[i] === '(' || str[i] === ')') returnedArr += ' ' + str[i] + ' ';
     else  returnedArr += (str[i]);
+    if (str[i] === '(') bracketCounter++;
+    if (str[i] === ')') bracketCounter--;
+  }
+  if (bracketCounter != 0){
+    console.log('Invalid brackets');
+    return false;
   }
   returnedArr = returnedArr.split(' ');
   returnedArr = returnedArr.filter(Boolean);
   for (let i = 0; i < returnedArr.length; ++i)
   {
-    if (Number(returnedArr[i])) returnedArr[i] = Number(returnedArr[i]);
+    if (Number(returnedArr[i]) || returnedArr[i] == '0') returnedArr[i] = Number(returnedArr[i]);
   }
   return returnedArr;
 }
 
 function splitAndDoExpression(expression) {
-  let num1, num2;
+  console.log(expression);
+  let action, num1, num2;
+  let operCount = 0;
+  let i = 0;
+  num1 = null;
+  num2 = null;
+  action = null;
 
-  num2 = readValue(expression);
-  if (Array.isArray(num2)) num2 = splitAndDoExpression(num2);
-
-  num1 = readValue(expression);
-  if (Array.isArray(num1)) num1 = splitAndDoExpression(num1);
-
-
-  if (num1 && num2){
-    if (expression[0] == '/'){
-      if (num2 != 0)
-        return num1 / num2;
+  while (expression.length > 0)
+  {
+    if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/'){
+      action = expression.shift();
+      operCount = 0;
+      continue;
+    }
+    if (num1 == null){
+      if (Number.isInteger(expression[i])) {
+        num1 = expression.shift();
+        ++operCount;
+        continue;
+      }
       else {
-        console.log("Division by 0!!!")
+        if (expression[i] == '('){
+          expression.shift();
+          num1 = splitAndDoExpression(expression);
+          if (!num1) break;
+          ++operCount;
+          continue;
+        }
+      }
+    }
+    if (num2 == null && num1 !== null) {
+      if (Number.isInteger(expression[i])) {
+        num2 = expression.shift();
+        ++operCount;
+        continue;
+      } else {
+        if (expression[i] == '(') {
+          expression.shift()
+          num2 = splitAndDoExpression(expression);
+          if (!num2) break;
+          ++operCount;
+          continue;
+        }
+      }
+    }
+    if (expression[i] == ')'){
+      expression.shift()
+      break;
+    }
+    if (num1 != null && num2 != null && expression[i] != ')'){
+      console.log('Extra operator!');
+      return false;
+    }
+  }
+
+  if (action && operCount == 2 && num1 != null && num2 != null){
+    if (action == "/"){
+      if (num2 !== 0) return num1 / num2;
+      else {
+        console.log("Division by 0!!!");
         return false;
       }
     }
-    if (expression[0] == '*') return num1 * num2;
-    if (expression[0] == '-') return num1 - num2;
-    if (expression[0] == '+') return num1 + num2;
+    if (action == '*') return num1 * num2;
+    if (action == '-') return num1 - num2;
+    if (action == '+') return num1 + num2;
   }
 
-  console.log("Invalid expression.");
   return false;
-}
-
-function readValue(arr) {
-  // console.log(arr);
-  let returnedArr = [];
-  let bracketCounter = 0;
-  let arraySize;
-  let buffer;
-  if (arr[(arr.length - 1)] != ')') {
-    buffer = arr.pop();
-    return buffer;
-  }
-  else {
-    bracketCounter = 1;
-    arr.pop();
-    arraySize = arr.length;
-    while (bracketCounter != 0) {
-      arraySize--;
-      if (arraySize == 0) {
-        console.log('Invalid brackets');
-        return false;
-      }
-      if (arr[arraySize] == '(') bracketCounter--;
-      if (arr[arraySize] == ')') bracketCounter++;
-      buffer = arr.pop();
-      if (bracketCounter != 0) {
-        returnedArr.unshift(buffer);
-      }
-    }
-  }
-  return returnedArr;
 }
 
 function calc(str) {
   let expression = [];
   if (str !== undefined) {
     expression = stringToArray(str);
-    expression = splitAndDoExpression(expression);
-    if (expression !== false)
-      console.log(expression);
+    if (expression) expression = splitAndDoExpression(expression);
+    if (expression) console.log(expression);
+    else console.log("Invalid expression.");
   }
   else
   {
